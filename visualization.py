9077c9540071fc96c66821f6ae4834b593a98120
gwyndigangi04 <digangigwyneth@gmail.com>
2024-11-30 23:03:52 -0500

    Scatter plots now have correct labels for each: "Peak Low Frequency", "Peak Mid Frequency", & "Peak High Frequency"

--- visualization.py
+++ visualization.py
@@ -63,25 +63,19 @@ def plot_timeseries_general(file_path, plot_type):
             peak_low_index = np.argmax(amplitude_spectrum_db[(freq >= 5) & (freq <= 300)])
             peak_low_freq = filtered_freq[peak_low_index]
             peak_low_value = filtered_amplitude_db[peak_low_index]
-            print(peak_low_freq) # Temporary display
-            print(peak_low_value)
 
             peak_mid_index = np.argmax(amplitude_spectrum_db[(freq >= 300) & (freq <= 2000)])
             peak_mid_freq = filtered_freq[peak_mid_index]
             peak_mid_value = filtered_amplitude_db[peak_mid_index]
-            print(peak_mid_freq)
-            print(peak_mid_value)
 
             peak_high_index = np.argmax(amplitude_spectrum_db[(freq >= 2000) & (freq <= np.max(freq))])
             peak_high_freq = filtered_freq[peak_high_index]
             peak_high_value = filtered_amplitude_db[peak_high_index]
-            print(peak_high_freq)
-            print(peak_high_value)
 
             # Plot a dot at the highest value within each frequency range
-            ax.scatter(1 / peak_low_freq, peak_low_value, color='green', s=100, label=f"Peak {plot_type}")
-            ax.scatter(1 / peak_mid_freq, peak_mid_value, color='yellow', s=100, label=f"Peak {plot_type}")
-            ax.scatter(1 / peak_high_freq, peak_high_value, color='red', s=100, label=f"Peak {plot_type}")
+            ax.scatter(1 / peak_low_freq, peak_low_value, color='green', s=100, label=f"Peak Low Frequency")
+            ax.scatter(1 / peak_mid_freq, peak_mid_value, color='yellow', s=100, label=f"Peak Mid Frequency")
+            ax.scatter(1 / peak_high_freq, peak_high_value, color='red', s=100, label=f"Peak High Frequency")
 
 
             fig.tight_layout(pad=2)
